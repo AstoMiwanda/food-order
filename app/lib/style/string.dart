44236255b63419a9feed9a6d886c49e8bbc325ab
Asto Arianto Miwanda <astomiwanda08@gmail.com>
2020-01-26 00:52:34 +0700

    make struktur directory

---
+++ app/lib/style/string.dart
@@ -0,0 +1,78 @@
+class Text {
+//  Label App
+  static var lblMainMenu              = "Main Menu";
+
+//  Title Text
+  static var titleMainMenu            = "Lapar?";
+  static var titleFood                = "Makanan";
+  static var titleSnack               = "Cemilan";
+  static var titleDrink               = "Minuman";
+  static var titleCart                = "Keranjang";
+  static var titleAddress             = "Dimana alamat anda?";
+  static var titleListOrder           = "List pesanan anda";
+  static var titleAllOrder            = "Total pesanan anda";
+  static var titleTrack               = "Sudah lapar?";
+  static var titleOrder               = "Pesan";
+  static var titleCooking             = "Masak";
+  static var titlePacking             = "Bungkus";
+  static var titleShipped             = "Dikirim";
+  static var titleArrived             = "Sampai";
+  static var titleRating              = "Penilaian:";
+
+//  Sub Title Text
+  static var subTitleMainMenu         = "Pesan makanan anda sekarang.";
+  static var subTitleFood             = "Mau makan apa hari ini?";
+  static var subTitleSnack            = "Mau ngemil apa hari ini?";
+  static var subTitleDrink            = "Mau minum apa hari ini?";
+  static var subTitleCart             = "Bayar dan nikmati pesanan anda.";
+  static var subTitleTrack            = "Periksa status pesanan anda.";
+
+//  Content Text
+  static var contentNote              = "Catatan:";
+  static var contentOrder             = "Pesanan anda sedang berada di antrian dapur.";
+  static var contentCooking           = "Pesanan anda sedang kami masak, tunggu beberapa menit lagi yaa...";
+  static var contentPacking           = "Pesanan anda berada dalam proses pengemasan, tidak lama lagi pesanan anda akan sampai...";
+  static var contentShipped           = "Pesanan anda berada dalam pengiriman, sebentar lagi anda dapat menikmati pesanan anda.";
+  static var contentArrived           = "Yeyy, pesanan anda telah sampai, selamat menikmati.";
+
+//  Placeholder Text
+  static var placeholderAddress       = "Tuliskan alamat dan nomer telepon anda.";
+  static var placeholderRating        = "Berikan penilaian anda terhadap pesanan.";
+
+//  List Category Text
+  static var categoryFood             = "Makanan";
+  static var categorySnack            = "Cemilan";
+  static var categoryDrink            = "Minuman";
+
+//  Button Text
+  static var btnChoose                = "Pilih";
+  static var btnOrder                 = "Pesan";
+  static var btnRating                = "Berikan Penilaian";
+}
+
+class Message {
+
+}
+
+class Images {
+
+}
+
+class Icons {
+//  Icon App
+  static var app                      = "assets/icons/app/";
+  static var appCart                  = app + "cart.svg";
+  static var appHome                  = app + "home.svg";
+  static var appNext                  = app + "next.svg";
+  static var appTracking              = app + "tracking.svg";
+
+//  Icon Tracking
+  static var tracking                 = "assets/icons/tracking/";
+  static var trackingArrived          = tracking + "arrived.svg";
+  static var trackingCooking          = tracking + "cooking.svg";
+  static var trackingNullStar         = tracking + "null-star.svg";
+  static var trackingOrder            = tracking + "order.svg";
+  static var trackingPacking          = tracking + "packing.svg";
+  static var trackingShipped          = tracking + "shipped.svg";
+  static var trackingStar             = tracking + "star.svg";
+}
---
+++ app/lib/style/theme.dart
@@ -0,0 +1,51 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Colors {
+  const Colors();
+
+  static const Color transparent = Color(0x00000000);
+//  OPACITY 50% white : 0x80FFFFFF
+//  100% — FF
+//  95% — F2
+//  90% — E6
+//  85% — D9
+//  80% — CC
+//  75% — BF
+//  70% — B3
+//  65% — A6
+//  60% — 99
+//  55% — 8C
+//  50% — 80
+//  45% — 73
+//  40% — 66
+//  35% — 59
+//  30% — 4D
+//  25% — 40
+//  20% — 33
+//  15% — 26
+//  10% — 1A
+//  5% — 0D
+//  0% — 00
+
+//  App
+  static const Color primaryApp = const Color(0xFF00C853);
+
+//  Food
+  static const Color primaryFood = const Color(0xFF4DD0E1);
+
+//  Snack
+  static const Color primarySnack = const Color(0xFFF9925A);
+  
+//  Drink
+  static const Color primaryDrink = const Color(0xFFF48FB1);
+
+//  Dark Text
+  static const Color darkTitle = const Color(0xFF424242);
+  static const Color darkContent = const Color(0xFF696969);
+  
+//  Light Text
+  static const Color lightTitle = const Color(0xFFFFFFFF);
+  static const Color lightSubTitle = const Color(0xFFF5F5F5);
+
+}
